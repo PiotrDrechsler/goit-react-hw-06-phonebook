b0a55543d23335c965e2d4c01bdfd9d6c7fade38
PiotrDrechsler <pdrech@gmail.com>
2023-02-08 14:15:33 +0100

    remove ex - add phonebook02

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,57 @@
+import { useState, useEffect } from 'react';
+
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+
+import s from '../App.module.css';
+
 export const App = () => {
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('contacts')) || []
+  );
+
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = event => {
+    const loweredCase = event.name.toLowerCase().trim();
+
+    const exists = contacts.some(
+      contact => contact.name.toLowerCase().trim() === loweredCase
+    );
+
+    if (exists) {
+      alert(`${event.name} is already in contacts!`);
+    } else {
+      setContacts([...contacts, event]);
+    }
+  };
+
+  const addFilter = event => {
+    setFilter(event.currentTarget.value);
+  };
+
+  const filteredContacts = contacts.filter(contact => {
+    return contact.name.toLowerCase().includes(filter.toLowerCase());
+  });
+
+  const deleteContact = id => {
+    const filtered = contacts.filter(contact => contact.id !== id);
+    setContacts(filtered);
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <section className={s.content}>
+      <div className={s.content__container}>
+        <ContactForm addContact={addContact} />
+        <ContactList contacts={filteredContacts} deleteContact={deleteContact}>
+          <Filter filter={filter} addFilter={addFilter} />
+        </ContactList>
+      </div>
+    </section>
   );
 };
